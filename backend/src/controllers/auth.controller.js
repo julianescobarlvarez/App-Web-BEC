a70343d2599365344d7d2d8e47d80d2d84317869
tinyyoro <164403702+humblelurker@users.noreply.github.com>
2024-11-14 08:41:43 -0300

    login y logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -32,4 +32,34 @@ export const register = async (req, res) => {
         res.status(500).json({ message: error.message});
     }
 }
-export const login = (req, res) => res.send('login');
+export const login = async (req, res) => {
+    const {email, contraseña} = req.body;
+    try{
+        const userFound = await User.findOne({email});
+        if (!userFound) return res.status(400).json({message: "Usuario no encontrado"});
+        const isMatch = await bcrypt.compare(contraseña, userFound.contraseña)
+
+        const passwordHash = await bcrypt.hash(contraseña, 11);
+        if (!isMatch) return res.status(400).json({message : "Contraseña incorrecta"});
+
+
+        const token = await createAcessToken({id: userFound._id})
+        res.cookie("token", token);
+        res.json({
+           id: userFound._id,
+           nombres: userFound.nombres,
+           email: userFound.email,
+           createdAt: userFound.createdAt,
+           updatedAt: userFound.updatedAt
+        });
+        
+    }   catch (error){
+        res.status(500).json({ message: error.message});
+    }
+}
+export const logout = (req, res) => {
+    res.cookie('token', "", {
+        expires: new Date(0)
+    });
+    return res.sendStatus(200);
+}
--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import {Router} from 'express'
-import {login, register} from '../controllers/auth.controller.js'
+import {login, register, logout} from '../controllers/auth.controller.js'
 const router = Router()
 
 router.post('/registro', register);
 router.post('/login', login);
+router.post('/logout', logout);
 
 export default router;
