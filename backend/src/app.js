c4977d16c4c2c5920eebf975edf077a5539d416c
tinyyoro <164403702+humblelurker@users.noreply.github.com>
2024-11-16 23:27:07 -0300

    instalación de cors
    a

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,9 +2,11 @@ import express from 'express'
 import morgan from 'morgan' // para ver las peticiones que llegan al backend
 import authRoutes from './routes/auth.routes.js'
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+app.use(cors()); // para que todos los dominios se puedan comunicar con este servidor
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
