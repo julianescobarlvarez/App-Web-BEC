03a14d46f0e3a0d714c68999ec6f002ef0f2f005
humblelurker <164403702+humblelurker@users.noreply.github.com>
2024-11-19 23:50:16 -0300

    Validación de documento

--- backend/src/routes/catalogo.routes.js
+++ backend/src/routes/catalogo.routes.js
@@ -7,12 +7,14 @@ import {
     UpdateDocument,
     DeleteDocument,
 }  from "../controllers/document.controller.js";
+import { validateSchema } from '../middlewares/validator.middleware.js';
+import { createDocumentSchema } from '../schemas/document.schema.js';
 
 const router= Router();
 
 router.get("/documents", authRequired, getDocuments);
 router.get("/documents/:id", authRequired, getDocument);
-router.post("/documents", authRequired, createDocument);
+router.post("/documents", authRequired, validateSchema(createDocumentSchema), createDocument);
 router.delete("/documents/:id", authRequired, DeleteDocument);
 router.put("/documents/:id", authRequired, UpdateDocument);
 export default router;
---
+++ backend/src/schemas/document.schema.js
@@ -0,0 +1,31 @@
+import {z} from 'zod';
+
+export const createDocumentSchema = z.object({
+    identificador: z.string({
+        required_error: 'El identificador es requerido'
+    }),
+    tipo: z.string({
+        required_error: 'El tipo de documento es requerido'
+    }),
+    titulo: z.string({
+        required_error: 'El título es requerido'
+    }),
+    autor: z.string({
+        required_error: 'El autor es requerido'
+    }),
+    editorial: z.string({
+        required_error: 'Editorial inválida'
+    }).optional(),
+    año: z.number({
+        required_error: 'El año es requerido'
+    }),
+    edicion: z.string({
+        required_error: 'Edición inválida'
+    }).optional(),
+    categoria: z.string({
+        required_error: 'La categoría es requerido'
+    }),
+    tipoMedio: z.string({
+        required_error: 'El tipo de medio es requerido'
+    })
+});
